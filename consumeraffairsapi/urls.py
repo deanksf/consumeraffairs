6a98e3f1133985c28be173c5d303f58c79c9f6a2
Dean <deankondo@DKs-Air.local>
2021-11-27 15:37:34 -0800

    Implement endpoints

--- consumeraffairsapi/urls.py
+++ consumeraffairsapi/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import include, path
 from rest_framework import routers
 from . import views
@@ -7,5 +8,8 @@ router.register(r'events', views.EventsViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^events$', views.events_create),
+    url(r'^events/(?P<pk>[0-9]+)$', views.events_detail),
+    url(r'^events/series$', views.events_series),
 ]
--- consumeraffairsapi/views.py
+++ consumeraffairsapi/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
 
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 
 from .serializers import EventsSerializer
 from consumeraffairs.models import Events
@@ -9,3 +11,33 @@ from consumeraffairs.models import Events
 class EventsViewSet(viewsets.ModelViewSet):
     queryset = Events.objects.all().order_by('action_timestamp')
     serializer_class = EventsSerializer
+
+
+@api_view(['POST'])
+def events_create(request):
+    events_data = JSONParser().parse(request)
+    events_serializer = EventsSerializer(data=events_data)
+    if events_serializer.is_valid():
+        # TODO: I would use a task queue w/ caching to save events to improve performance at "scale".
+        events_serializer.save()
+        return JsonResponse(events_serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def events_detail(request, pk):
+    try:
+        event = Events.objects.get(pk=pk)
+    except Events.DoesNotExist:
+        return JsonResponse({'message': 'The event does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        event_serializer = EventsSerializer(event)
+        return JsonResponse(event_serializer.data)
+
+
+# TODO: GET by session_id (user), entity_id (site, app, etc.) or
+#  action_name/category, sorted by date_range (default to last 30 days)
+@api_view(['GET'])
+def events_series(request):
+    content = {'message': 'Series not implemented'}
+    return JsonResponse(content, status=status.HTTP_501_NOT_IMPLEMENTED)
